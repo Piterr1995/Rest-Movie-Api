12efe83f3fa059b48ca9164d07f4087929a3a31f
Piotr <mojprogress@gmail.com>
2020-10-07 22:43:33 +0200

    delete staticfiles

--- restapi/settings.py
+++ restapi/settings.py
@@ -130,5 +130,9 @@ USE_TZ = True
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/staticfiles/"
+STATIC_URL = "/static/"
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
