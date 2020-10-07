a1ac49dca617ad50f68116c93e09fb9aefd3da43
Piotr <mojprogress@gmail.com>
2020-10-07 22:49:28 +0200

    added static urls and root

--- restapi/settings.py
+++ restapi/settings.py
@@ -132,7 +132,4 @@ USE_TZ = True
 CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_URL = "/staticfiles/"
