9b5b44ec8a9edaa4f1687ef94f4588fe6daea856
Piotr <mojprogress@gmail.com>
2020-10-08 08:10:32 +0200

    changed heroku settings

--- restapi/settings.py
+++ restapi/settings.py
@@ -134,7 +134,4 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # Heroku recommended settings
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_URL = "/staticfiles/"
